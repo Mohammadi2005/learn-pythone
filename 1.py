160fe2d679c87370ce86590167b604107f35311b
Mohammadi2005 <159756424+Mohammadi2005@users.noreply.github.com>
2024-07-18 11:43:16 +0330

    first coed

---
+++ 1.py
@@ -0,0 +1,87 @@
+print('hello world')
+print("im amir hossein")
+
+# date type
+
+print (type ('hello world')) # string
+
+print (183)
+print (type (183)) # int
+
+print (15.4)
+print (type (15.4)) # float
+
+print (75j)
+print (type (75j)) # complex
+
+# operator
+
+print ( 54 + 6 )
+print ( 24 - 8 - 7)
+print ( 4 * 3 )
+print ( 42 / 3 )
+print ( type ( 42 / 3 ))
+print ( 8 % 3 )
+print ( type (48 % 5))
+
+#  power
+
+print ( 4 ** 3 )
+
+print ( 8 / 3 )
+
+print ( 8 // 3 )
+print ( type ( 8 // 3 ))
+
+# variable
+
+x = 5
+print (x)
+print ( type (x))
+
+y = "ali"
+print (y)
+print ( type (y))
+
+z = 2.6
+print (z)
+print ( type (z))
+
+x = 49
+print (x)
+
+name = 'ali'
+Name = 'reza'
+
+print (name)
+print (Name)
+
+# Casting # ریخته گری
+ 
+x = int(86)
+print (x)
+
+y = int(3.7) # y will be 3
+print (y)
+
+z = int("245") # z will be 245
+print (z)
+
+X = float(86) # X will be 86.0
+print (X)
+
+Y = float(3.7) # Y will be 3.7
+print (Y)
+
+Z = float("245") # Z will be 245.0
+print (Z)
+
+X_str = str(86)
+print (X_str)
+
+Y_str = str(3.7)
+print (Y_str)
+
+Z_str = str("245")
+print (Z_str)
+

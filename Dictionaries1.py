d5b35616bc453ee72abe7a814d2fe2eebca8f822
Mohammadi2005 <159756424+Mohammadi2005@users.noreply.github.com>
2024-07-20 07:49:25 +0330

    if else and dictionaries

---
+++ Dictionaries1.py
@@ -0,0 +1,23 @@
+#  روش دوم تعریف ساختار های تو در تو
+
+child1 = {
+    "name": "ahmad reza",
+    "age": 22
+}
+child2 = {
+    "name": "amir hossein",
+    "age": 20
+}
+child3 = {
+    "name": "fateme",
+    "age": 10
+}
+
+myfamily = {
+
+    "name fader": "mohammad mohammadi",
+
+    "child1": child1,
+    "child2": child2,
+    "child3": child3
+}
---
+++ IfElse.py
@@ -0,0 +1,26 @@
+age_amir = 30
+age_ahmad = 3
+
+if age_amir < age_ahmad:
+    print("ahmad is bigger then amir")
+
+elif age_amir > age_ahmad:
+    print("amir is bigger then ahmad")
+
+else:
+    print("amir and ahmad are equal")
+
+x = 12
+y = 9
+
+if x > y and y > 10:
+    print("x is bigger then 10")
+
+elif y > x or y > 10:
+    print("y is bigger then x or y is beger then 10")
+
+elif y == x and y > age_ahmad or age_amir > 15:
+    pass
+
+else:
+    print("......")
